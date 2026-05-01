ed4f7ef3908b5096700ea9fc194452382e815c30
Nikki7150 <kanchan.nishka@gmail.com>
2026-05-01 09:10:36 -0700

    finished code

--- src/main/java/edu/sdccd/cisc191/GenericMatchQueue.java
+++ src/main/java/edu/sdccd/cisc191/GenericMatchQueue.java
@@ -7,23 +7,33 @@ public class GenericMatchQueue<T> {
     private final LinkedList<T> items = new LinkedList<>();
 
     public void enqueue(T item) {
-        // TODO: add the item to the back of the queue
+        // add the item to the back of the queue
+        items.add(item);
     }
 
     public T dequeue() {
-        // TODO: remove and return the front item
+        // remove and return the front item
         // throw IllegalStateException if the queue is empty
-        return null;
+        if (items.isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return items.remove();
     }
 
     public T peek() {
-        // TODO: return the front item without removing it
+        // return the front item without removing it
         // throw IllegalStateException if the queue is empty
-        return null;
+        if (items.isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return items.peek();
     }
 
     public boolean isEmpty() {
-        // TODO: return true when the queue has no items
+        // return true when the queue has no items
+        if (items.isEmpty()) {
+            return true;
+        }
         return false;
     }
 
